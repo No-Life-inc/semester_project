ac0f750c4c6862dab6036fe2ab89aa6ab804a33c
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-07 12:26:30 +0100

    refactored ci

--- backend/__tests__/intergrationTests/collection.test.ts
+++ backend/__tests__/intergrationTests/collection.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, expect, it, jest } from "@jest/globals";
+import { afterAll, beforeAll, afterEach, describe, expect, it, jest } from "@jest/globals";
 
 jest.setTimeout(120000);
 
