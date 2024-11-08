ceaa3f5183ae0a9635c8d1734774027a73846e8b
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-08 11:50:38 +0100

    added comments to collection routes

--- backend/routes/V1/collectionRoutes.ts
+++ backend/routes/V1/collectionRoutes.ts
@@ -3,12 +3,22 @@ import * as CollectionController from "../../controllers/collectionController";
 
 const router = Router();
 
+// Route to get all collections for a specific user
 router.get("/:userId/", CollectionController.getUserCollections);
-router.post("/", CollectionController.createCollection);
-router.put("/:userId/:id", CollectionController.updateCollection);
-router.delete("/:userId/:id", CollectionController.deleteCollection);
-router.post("/addBook", CollectionController.addBookToCollection);
-router.delete("/removeBook", CollectionController.removeBookFromCollection);
 
+// Route to create a new collection
+router.post("/", CollectionController.createCollection);
+
+// Route to update a collection for a user
+router.put("/:userId/:id", CollectionController.updateCollection);
+
+// Route to delete a collection for a user
+router.delete("/:userId/:id", CollectionController.deleteCollection);
+
+// Route to add a book to a collection
+router.post("/addBook", CollectionController.addBookToCollection);
+
+// Route to remove a book from a collection
+router.delete("/removeBook", CollectionController.removeBookFromCollection);
 
 export default router;
