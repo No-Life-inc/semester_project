d2cbf1ed93279b18f33a2c7d9a17153d96187ebb
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-13 10:56:12 +0100

    full remove of auth0 + packages

--- backend/controllers/userController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export const handleCallback = (req: Request, res: Response): void => {
-  res.redirect("/dashboard");
-};
-
-export const getDashboard = (req: Request, res: Response): void => {
-  res.send("Hello from dashboard!");
-};
