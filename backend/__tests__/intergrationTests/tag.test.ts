748137e537aa26db7c8efe1e21e8527b3534e141
ZackO <zo@pyra.dk>
2024-11-13 10:21:15 +0100

    tag test update

--- backend/__tests__/intergrationTests/tag.test.ts
+++ backend/__tests__/intergrationTests/tag.test.ts
@@ -71,6 +71,12 @@ describe("addTag function tests", () => {
     test("should throw an error when creating a tag without a name", async () => {
         await expect(addTag("")).rejects.toThrow("Tag name is required");
     });
+    test("should throw an error when Tag.create fails", async () => {
+        const longName = "a".repeat(256);
+        await expect(addTag(longName)).rejects.toThrow("An error occurred while creating tag");
+    });
+
+
 });
 
 
