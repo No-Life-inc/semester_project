45da12a21976cddb3c50509ec6afc0017bc983f6
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-14 15:25:28 +0100

    fixed refactor_collection migration file and removed user_collections from setupTestDB

--- backend/database/knex/migrations/20241113113011_refactor_collection.ts
+++ backend/database/knex/migrations/20241113113011_refactor_collection.ts
@@ -1,25 +1,24 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.table('collections', table => {
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.id');
-    });
-    await knex.schema.dropTable('user_collections');
+  await knex.schema.table("collections", (table) => {
+    table.integer("user_id").unsigned().notNullable();
+    table.foreign("user_id").references("users.id");
+  });
+  await knex.schema.dropTable("user_collections");
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.createTable('user_collections', table => {
-        table.increments('id').primary();
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.id');
-        table.integer('collection_id').unsigned().notNullable();
-        table.foreign('collection_id').references('collections.id');
-    });
-    await knex.schema.table('collections', table => {
-        table.dropColumn('user_id');
-    });
+  await knex.schema.createTable("user_collections", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").unsigned().notNullable();
+    table.foreign("user_id").references("users.id");
+    table.integer("collection_id").unsigned().notNullable();
+    table.foreign("collection_id").references("collections.id");
+    table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
+  });
+  await knex.schema.table("collections", (table) => {
+    table.dropForeign(["user_id"]);
+    table.dropColumn("user_id");
+  });
 }
-
