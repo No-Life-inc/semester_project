1a04d29422b43a2ae40bae98cb0ee0736175a4cf
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-25 10:08:40 +0100

    zack fixed the route problem

--- backend/routes/V1/collectionRoutes.ts
+++ backend/routes/V1/collectionRoutes.ts
@@ -12,16 +12,17 @@ router.get("/", CollectionController.getUserCollections);
 // Route to create a new collection
 router.post("/", CollectionController.createCollection);
 
+// Route to add a book to a collection
+router.post("/addBook", CollectionController.addBookToCollection);
+
+// Route to remove a book from a collection
+router.delete("/removeBook", CollectionController.removeBookFromCollection);
+
 // Route to update a collection for a user
 router.put("/:id", CollectionController.updateCollection);
 
 // Route to delete a collection for a user
 router.delete("/:id", CollectionController.deleteCollection);
 
-// Route to add a book to a collection
-router.post("/addBook", CollectionController.addBookToCollection);
-
-// Route to remove a book from a collection
-router.delete("/removeBook", CollectionController.removeBookFromCollection);
 
 export default router;
