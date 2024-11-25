677a7344fee6ac2f37c589b2e671b231f5ffafb0
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-25 08:53:23 +0100

    fixed routes

--- backend/routes/V1/collectionRoutes.ts
+++ backend/routes/V1/collectionRoutes.ts
@@ -7,16 +7,16 @@ const router = Router();
 router.use(authenticate);
 
 // Route to get all collections for a specific user
-router.get("/:userId/", CollectionController.getUserCollections);
+router.get("/", CollectionController.getUserCollections);
 
 // Route to create a new collection
 router.post("/", CollectionController.createCollection);
 
 // Route to update a collection for a user
-router.put("/:userId/:id", CollectionController.updateCollection);
+router.put("/:id", CollectionController.updateCollection);
 
 // Route to delete a collection for a user
-router.delete("/:userId/:id", CollectionController.deleteCollection);
+router.delete("/:id", CollectionController.deleteCollection);
 
 // Route to add a book to a collection
 router.post("/addBook", CollectionController.addBookToCollection);
