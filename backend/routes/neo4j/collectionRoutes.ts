8f3efe4f780cb81675689dbac391f7860267e561
Morten Bendeke <mbendeke@hotmail.com>
2024-11-27 14:48:16 +0100

    subjects gets guids now

--- backend/routes/neo4j/collectionRoutes.ts
+++ backend/routes/neo4j/collectionRoutes.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import {getUserCollectionsController, createCollectionController, updateCollectionController, deleteCollectionController, addBookToCollectionController, removeBookFromCollectionController } from "../../controllers/neo4j/collectionController";
+import {authenticate} from "../../utility/authMiddleware";
 
 const router = express.Router();
+
+router.use(authenticate);
 
 router.get("/", getUserCollectionsController);
 router.post("/", createCollectionController);
