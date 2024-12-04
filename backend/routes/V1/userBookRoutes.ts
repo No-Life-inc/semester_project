8d213014d5a2070e3c4feb5e4773a703477b54f6
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-12-04 13:05:12 +0100

    added authenticate on routes

--- backend/routes/V1/userBookRoutes.ts
+++ backend/routes/V1/userBookRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { getUserBooksController, addBookToUserController, removeBookFromUserController } from "../../controllers/userBookController";
+import { authenticate } from "../../utility/authMiddleware";
 
 const router = express.Router();
 
+router.use(authenticate);
 router.get("/", getUserBooksController);
 router.post("/", addBookToUserController);
 router.delete("/:userBookId", removeBookFromUserController);
